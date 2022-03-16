1871aa68b5bc6b92541449cfc2b508dff670581b
heck <ramazan280699@gmail.com>
2022-03-17 00:13:21 +0300

    modified.v2

--- vms/Ubuntu_A/application/application.py
+++ vms/Ubuntu_A/application/application.py
@@ -1,19 +1,21 @@
+from flask import request
 from flask import Flask
+from crypt import methods
 
 app = Flask(__name__)
 
-@app.route("/get")
-def hello_word_get():
-	return "<p>Hello, Word from get!</p>"
+@app.route("/", methods=['GET'])
+def hello_word():
+	return "<p>Hello, Word!</p>\n"
 
 
-@app.route("/put")
-def hello_word_put():
-	return "<p>Hello, Word from put!</p>"
+@app.route("/change_login", methods=['PUT'])
+def change_login():
+	return f"<p>new login is {request.form['login']}!</p>\n"
 
 
-@app.route("/post")
-def hello_word_pust():
-	return "<p>Hello, Word from post!</p>"
+@app.route("/login", methods=['POST'])
+def login():
+	return f"<p>Your login is {request.form['login']}</p>\n"
 
 app.run(host='0.0.0.0', port=5000)
